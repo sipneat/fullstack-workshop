e22a3efeb8f1a8bf023fd3dbc18f812ab0e222ab
sipneat <jesperson@scu.edu>
2025-05-17 19:01:05 +0000

    another change

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-5xl">You know you want to work!</h1>
+      <h1 className="text-5xl">You know you want to work! yay!</h1>
       <p>Counter = {counter}</p>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
